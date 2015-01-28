786c842b9b05fb19c924a62e26477d828a51f3a5
Jacob Atzen <jatzen@gmail.com>
2015-01-28 14:43:27 +0100

    Reuse handler

--- config/initializers/cqrs.rb
+++ config/initializers/cqrs.rb
@@ -5,18 +5,21 @@ $db = SimpleCQRS::BullShitDatabase.new
 
 storage = SimpleCQRS::EventStore.new($bus)
 rep = $rep = SimpleCQRS::Repository.new(storage, SimpleCQRS::InventoryItem)
+
 commands = SimpleCQRS::InventoryCommandHandlers.new(rep)
-$bus.register_command_handler(SimpleCQRS::CheckInItemsToInventory, SimpleCQRS::InventoryCommandHandlers.new(rep))
-$bus.register_command_handler(SimpleCQRS::CreateInventoryItem, SimpleCQRS::InventoryCommandHandlers.new(rep))
-$bus.register_command_handler(SimpleCQRS::DeactivateInventoryItem, SimpleCQRS::InventoryCommandHandlers.new(rep))
-$bus.register_command_handler(SimpleCQRS::RemoveItemsFromInventory, SimpleCQRS::InventoryCommandHandlers.new(rep))
-$bus.register_command_handler(SimpleCQRS::RenameInventoryItem, SimpleCQRS::InventoryCommandHandlers.new(rep))
+$bus.register_command_handler(SimpleCQRS::CheckInItemsToInventory, commands)
+$bus.register_command_handler(SimpleCQRS::CreateInventoryItem, commands)
+$bus.register_command_handler(SimpleCQRS::DeactivateInventoryItem, commands)
+$bus.register_command_handler(SimpleCQRS::RemoveItemsFromInventory, commands)
+$bus.register_command_handler(SimpleCQRS::RenameInventoryItem, commands)
+
 detail = SimpleCQRS::InventoryItemDetailView.new($db)
 $bus.register_event_handler(SimpleCQRS::InventoryItemCreated, detail)
 $bus.register_event_handler(SimpleCQRS::InventoryItemDeactivated, detail)
 $bus.register_event_handler(SimpleCQRS::InventoryItemRenamed, detail)
 $bus.register_event_handler(SimpleCQRS::ItemsCheckedInToInventory, detail)
 $bus.register_event_handler(SimpleCQRS::ItemsRemovedFromInventory, detail)
+
 list = SimpleCQRS::InventoryListView.new($db)
 $bus.register_event_handler(SimpleCQRS::InventoryItemCreated, list)
 $bus.register_event_handler(SimpleCQRS::InventoryItemRenamed, list)
